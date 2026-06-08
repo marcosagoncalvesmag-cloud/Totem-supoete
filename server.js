d02010ed0b1a56a36b5f679d87d638933909d5de
marcosagoncalvesmag-cloud <marcosagoncalvesmag@gmail.com>
2026-06-08 13:00:18 -0300

    Update server.js

--- server.js
+++ server.js
@@ -1,1 +1,14 @@
+const express = require("express");
+const app = express();
 
+app.use(express.json());
+
+app.get("/", (req, res) => {
+  res.send("Servidor Cachaça Express rodando 🍻");
+});
+
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+  console.log("Servidor rodando na porta " + PORT);
+});
